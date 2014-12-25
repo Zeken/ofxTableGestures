85b4fdb679ee369a06f537168a8ed240ecd615f9
Zeken <eszeken@hotmail.com>
2014-12-25 11:16:04 +0100

    Overload registerEvent() to factor out common functionality

--- src/Core/EventClient.hpp
+++ src/Core/EventClient.hpp
@@ -205,6 +205,7 @@ class EventClientObject
 /// to take events:
 ///     registerEvent( Event , &MyClass::method ); //listener is the same instance
 ///     registerEvent( Event , &MyClass::method , Listener );
+///     registerEvent( Event , &MyClass::method , Listener , Graphic ); // Only report events colliding with Graphic
 ///
 /// to take events where its target is this instance:
 ///     registerMyEvent( Event , &MyClass::method ); //listener is the same instance
@@ -214,20 +215,26 @@ class EventClient : EventClientObject
 {
     public:
     template <class EventType,typename ArgumentsType, class ListenerClass>
-    void registerEvent(EventType & event, void (ListenerClass::*listenerMethod)(ArgumentsType&),ListenerClass * listener =NULL)
-    {
-        if(listener == NULL)
+    void registerEvent(EventType & event,
+                    void (ListenerClass::*listenerMethod)(ArgumentsType&),
+                    ListenerClass * listener = NULL,
+                    Graphic* graphic = NULL){
+        if(listener == NULL){
             listener = static_cast<ListenerClass *>(this);
-        this->EventClientObject::registerEvent(listener,event,listenerMethod);
+        }
+        if (graphic == NULL){
+            this->EventClientObject::registerEvent(listener, event, listenerMethod);
+        }else{
+            this->EventClientObject::registerEventFiltered(listener, event, listenerMethod, graphic);
+        }
+
     }
-    
+
     template <class EventType,typename ArgumentsType, class ListenerClass>
-    void registerMyEvent(EventType & event, void (ListenerClass::*listenerMethod)(ArgumentsType&),ListenerClass * listener =NULL)
-    {
-        if(listener == NULL)
-            listener = static_cast<ListenerClass *>(this);
-        Graphic * target = (Graphic*)(this);
-        this->EventClientObject::registerEventFiltered(listener,event,listenerMethod,target);
+    void registerMyEvent(EventType & event,
+                        void (ListenerClass::*listenerMethod)(ArgumentsType&),
+                        ListenerClass * listener = NULL){
+        registerEvent(event, listenerMethod, listener, dynamic_cast<Graphic*>(this));
     }
     
     template <class EventType>
